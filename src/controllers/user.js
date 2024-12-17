9b466f01f89851f9fc9516edf359e4be7d1ee285
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-17 18:31:52 +0300

    created the structure of controller/user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,2 +1,36 @@
 "use strict"
 
+const User = require("../models/user")
+
+module.exports = {
+    list: async(req, res) => {
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    create: async(req, res) => {
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    read: async(req, res) => {
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    update: async(req, res) => {
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    delete: async(req, res) => {
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+}
