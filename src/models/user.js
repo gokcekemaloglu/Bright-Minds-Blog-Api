d45b74d6df38f6976228c192346612d115c11130
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-17 18:05:47 +0300

    created user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,2 +1,75 @@
 "use strict"
 
+const {mongoose} = require("../configs/dbConnection")
+
+/* ------------------------------------------------------- *
+{
+    "username": "admin",
+    "password": "aA?123456",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": true
+}
+/* ------------------------------------------------------- */
+
+const Userschema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    },
+
+    firstName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    lastName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+
+    isStaff: {
+        type: Boolean,
+        default: false
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+}, {
+    collection: 'users',
+    timestamps: true
+})
+
+/* ------------------------------------------------------- */
+
+module.exports = mongoose.model("User", Userschema)
