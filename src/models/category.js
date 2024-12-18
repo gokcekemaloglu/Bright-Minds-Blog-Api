339dee0a611c8a0b50d33173484019c0de5ca44a
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-18 14:14:17 +0300

    Added unique name field to Category model and exported it as a mongoose model.

--- src/models/category.js
+++ src/models/category.js
@@ -1,8 +1,20 @@
 "use strict"
 
 const {mongoose} = require("../configs/dbConnection")
+/* ------------------------------------------------------- */
+// Category Model:
 
-const CategorySchema = new mongoose.Schema({}, {
+const CategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    }
+}, {
     collection: "categories",
     timestamps: true
 })
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Category", CategorySchema)
