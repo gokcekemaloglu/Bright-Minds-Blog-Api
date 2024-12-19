318bab546901fbc325be8314a792d8491e7ceff0
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-19 16:12:16 +0300

    Added comment API endpoints and model

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -1,2 +1,79 @@
 "use strict"
 
+const Comment = require("../models/comment")
+
+module.exports = {
+    list: async(req, res) => {
+        /* 
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "List Comments"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    create: async(req, res) => {
+        /* 
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "Create Comment"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref:"#/definitions/Comment"
+                }
+            }
+        */
+        res.status(201).send({
+            error: false,
+            result
+        })
+    },
+    read: async(req, res) => {
+        /* 
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "Read Comment"
+        */
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    update: async(req, res) => {
+        /* 
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "Update Comment"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref:"#/definitions/Comment"
+                }
+            }
+        */
+        res.status(202).send({
+            error: false,
+            result
+        })
+    },
+    delete: async(req, res) => {
+        /* 
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "Delete Comment"
+        */
+        res.status(204).send({
+            error: false,
+            result
+        })
+    },
+}
--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,2 +1,19 @@
 "use strict"
 
+const router = require("express").Router()
+/* ------------------------------------------------------- */
+// routes/comment:
+
+const comment = require("../controllers/comment")
+
+router.route("/")
+    .get(comment.list)
+    .post(comment.create)
+router.route("/:id")
+    .get(comment.read)
+    .put(comment.update)
+    .patch(comment.update)
+    .delete(comment.delete)
+/* ------------------------------------------------------- */
+// Exports:
+module.exports = router
