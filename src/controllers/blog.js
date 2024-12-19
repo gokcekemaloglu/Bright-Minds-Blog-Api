c120aff09667e63f8c1f9df27c08b92656d9c179
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-19 14:27:13 +0300

    Added blog controller and model with basic CRUD operations and API documentation

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,2 +1,79 @@
 "use strict"
 
+const Blog = require("../models/blog")
+
+module.exports = {
+    list: async(req, res) => {
+        /* 
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "List Blogs"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    create: async(req, res) => {
+        /* 
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Create Blog"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref:"#/definitions/Blog"
+                }
+            }
+        */
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    read: async(req, res) => {
+        /* 
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Read Blog"
+        */
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    update: async(req, res) => {
+        /* 
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Update Blog"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref:"#/definitions/Blog"
+                }
+            }
+        */
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    delete: async(req, res) => {
+        /* 
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Delete Blog"
+        */
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+}
--- src/models/blog.js
+++ src/models/blog.js
@@ -1,2 +1,65 @@
 "use strict"
 
+const {mongoose} = require("../configs/dbConnection")
+
+const BlogSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    },
+
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    content: {
+        type: String,
+        required: [true, 'Content is required!'],
+        trim: true
+    },
+
+    image: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    isPublish: {
+        type: Boolean,
+        default: true
+    },
+
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ],
+
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+
+    countOfVisitors: {
+        type: Number,
+        default: 0
+    },
+}, {
+    collection: "blogs",
+    timestamps: true
+})
+
+module.exports = mongoose.model("Blog", BlogSchema)
+
